040fb4488fd21f30e8286656e6cb36ce58f62d1f
Wesley Whitney <wesleywhitney@gmail.com>
2019-03-10 20:11:35 -0600

    changes to test ci

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. And, also I changed this up.
+            Edit <code>src/App.js</code> and save to reload. And, also I changed this up, and now this to test the travis CI.
           </p>
           <a
             className="App-link"
